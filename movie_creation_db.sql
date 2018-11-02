c654648458f8d25f9a9e33e6e5e53778fb343bd2
cgrutzmacher <chris.grutzmacher@gmail.com>
2018-11-01 20:56:53 -0400

    initial checkin

---
+++ movie_creation_db.sql
@@ -0,0 +1,46 @@
+DROP DATABASE IF EXISTS bmdb;
+CREATE DATABASE bmdb;
+USE bmdb;
+
+
+CREATE TABLE movie ( 
+	ID					INT				PRIMARY KEY		AUTO_INCREMENT,
+    TITLE				VARCHAR(100)	NOT NULL,
+    GENRE				INT				NOT NULL,
+    RATING				VARCHAR(5)		NOT NULL,
+    `YEAR`				INT				NOT NULL,
+    DIRECTOR			VARCHAR(50)		NOT NULL
+
+);
+
+CREATE TABLE genre ( 
+	ID					INT				PRIMARY KEY		AUTO_INCREMENT,
+    genreTitle			VARCHAR(12)		NOT NULL
+);
+
+CREATE TABLE artist ( 
+	ID					INT				PRIMARY KEY		AUTO_INCREMENT,
+    firstName			VARCHAR(25)		NOT NULL,
+    lastName			VARCHAR(25)		NOT NULL,
+    gender				VARCHAR(6)		NOT NULL,	
+    birthday			DATE			NOT NULL    
+);
+
+CREATE TABLE movieGenre (
+	ID					INT				PRIMARY KEY		AUTO_INCREMENT,
+    movieID				INT				NOT NULL,
+    genreID				INT				NOT NULL,    
+    FOREIGN KEY (movieID) REFERENCES movie (ID),
+    FOREIGN KEY (genreID) REFERENCES genre (ID)
+);
+
+CREATE TABLE credit ( 
+	ID					INT				PRIMARY KEY		AUTO_INCREMENT,
+    movieID				INT,
+    actorID				INT,
+    characterName		VARCHAR(50),
+    FOREIGN KEY (movieID) REFERENCES movie (ID),
+    FOREIGN KEY (actorID) REFERENCES artist (ID)
+);
+
+
